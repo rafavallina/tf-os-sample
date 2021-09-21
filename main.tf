f513068cb288ec7ce71cff753abaf7f13d764f35
awishn02 <aaronwishnick@gmail.com>
2021-09-21 10:43:26 -0700

    Move to remote state

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+    bucket = "wishnick-terraform-states"
+    key    = "tf-os-sample/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
 # Configure the AWS Provider
