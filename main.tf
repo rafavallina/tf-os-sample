04ad031cc7b9988b58e5c557aef690aa9d8bae6e
awishn02 <aaronwishnick@gmail.com>
2021-09-21 10:09:49 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_ssm_parameter" "foo_test" {
+  name  = "foo"
+  type  = "String"
+  value = "bar"
+}
