76d6e407f9a08332fb20cdbefe8c59af47e3eaa4
awishn02 <aaronwishnick@gmail.com>
2021-09-30 16:10:51 -0700

    Update main.tf

--- dev/my-service/main.tf
+++ dev/my-service/main.tf
@@ -20,5 +20,5 @@ provider "aws" {
 resource "aws_ssm_parameter" "service_param" {
   name  = "service_param"
   type  = "String"
-  value = "service_param_value"
+  value = "service_param_value_1"
 }
