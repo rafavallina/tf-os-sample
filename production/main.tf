a32a156acec5dbb5c08938ef567ec167579ad967
awishn02 <aaronwishnick@gmail.com>
2021-09-30 14:00:18 -0700

    Create main.tf

---
+++ production/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "wishnick-terraform-state-bucket-2"
+    key    = "tf-os-sample/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_ssm_parameter" "foo_test" {
+  name  = "foo_test"
+  type  = "String"
+  value = "bar_1"
+}
