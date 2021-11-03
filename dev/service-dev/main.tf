dbd7c3846127ce48e88327602e3c68e19e8f166e
awishn02 <aaronwishnick@gmail.com>
2021-11-02 17:39:16 -0700

    add instance definition

---
+++ dev/service-dev/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "wishnick-terraform-states"
+    key    = "tf-os-sample/service-dev/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_ssm_parameter" "service_ssm_param" {
+  name  = "service_ssm_param"
+  type  = "String"
+  value = "value"
+}
